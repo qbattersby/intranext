647c49e58f7c58b16cd1b206d22fe130be160ac2
Quinn Battersby <qbattersby@me.com>
2015-12-18 21:16:37 -0500

    Added more pages, did responsive testing, need to complete remaining pages, borked CTA styles

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var compass = require('gulp-compass');
+var browserSync = require('browser-sync').create();
 
 // Default task
-gulp.task('default', function() {
-    gulp.src('sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+//gulp.task('default', function() {
+//    gulp.src('sass/*.scss')
+//        .pipe(sass().on('error', sass.logError))
+//        .pipe(gulp.dest('./css/'));
+//});
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve'], browserSync.reload);
 
 
 // Watch task
 gulp.task('watch', function() {
-    gulp.watch('sass/*.scss',['default']);
+    gulp.watch('sass/*.scss',['sass']);
 })
--- js/main.js
+++ js/main.js
@@ -9,6 +9,91 @@ $(document).ready(function(){
 
         $(this).addClass('active');
         $("#"+tab_id).addClass('active');
-    })
+    });
 
-})
+    $(function(){
+        $('nav').data('size','big');
+    });
+
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 30)
+        {
+            if($('nav').data('size') == 'big')
+            {
+                $('nav').data('size','small');
+                $('nav').stop().animate({
+                    height:'72px',
+                    top: "0",
+                    'line-height': '72px',
+                },50);
+                $('#intranext-logo').stop().animate({
+                    width: '132px',
+                    'margin-top': '15px',
+                },50);
+                $('.nav').stop().animate({
+                    'margin-top': '0px',
+                },50);
+                $('.nav li a.active, .nav li a:hover').stop().animate({
+                    'border-bottom-width': '0px',
+                },50);
+                $('nav').toggleClass('scroll-nav');
+            }
+        }
+        else if ($(document).scrollTop() < 29)
+        {
+            if($('nav').data('size') == 'small')
+            {
+                $('nav').data('size','big');
+                $('nav').stop().animate({
+                    height:'125px',
+                    top: "30",
+                    'line-height': '75px'
+                },50);
+                $('#intranext-logo').stop().animate({
+                    width: '195px',
+                    'margin-top': '33px',
+                },50);
+                $('.nav').stop().animate({
+                    'margin-top': '48px',
+                },50);
+                $('.nav li a.active, .nav li a:hover').stop().animate({
+                    'border-bottom-width': '2px',
+                },50);
+                $('nav').toggleClass('scroll-nav');
+            }
+        }
+    });
+
+
+    if($(document).scrollTop() > 30)
+    {
+        if($('nav').data('size') == 'big')
+        {
+            $('nav').data('size','small');
+            $('nav').stop().animate({
+                height:'72px',
+                top: "0",
+                'line-height': '72px',
+            },1);
+            $('#intranext-logo').stop().animate({
+                width: '132px',
+                'margin-top': '15px',
+            },1);
+            $('.nav').stop().animate({
+                'margin-top': '0px',
+            },1);
+            $('.nav li a.active, .nav li a:hover').stop().animate({
+                'border-bottom-width': '0px',
+            },1);
+            $('nav').toggleClass('scroll-nav');
+        }
+    }
+
+
+    $( ".menu-toggle" ).click(function(e) {
+        e.preventDefault();
+        $( ".mobile-menu" ).slideToggle('slow');
+    });
+
+
+});
